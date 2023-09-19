368fdb134b02ccf20370f7a9d0d11f8275a75697
liamhelmer <liam.helmer@mogo.ca>
2023-09-19 12:14:09 -0700

    Comment out local provider configuration

--- config/provider.tf
+++ config/provider.tf
@@ -1,24 +1,24 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-provider "oci" {
-  region               = var.region
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
-  private_key_password = var.private_key_password
-}
+#provider "oci" {
+#  region               = var.region
+#  tenancy_ocid         = var.tenancy_ocid
+#  user_ocid            = var.user_ocid
+#  fingerprint          = var.fingerprint
+#  private_key_path     = var.private_key_path
+#  private_key_password = var.private_key_password
+#}
 
-provider "oci" {
-  alias                = "home"
-  region               = local.regions_map[local.home_region_key]
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
-  private_key_password = var.private_key_password
-}
+#provider "oci" {
+#  alias                = "home"
+#  region               = local.regions_map[local.home_region_key]
+#  tenancy_ocid         = var.tenancy_ocid
+#  user_ocid            = var.user_ocid
+#  fingerprint          = var.fingerprint
+#  private_key_path     = var.private_key_path
+#  private_key_password = var.private_key_password
+#}
 
  terraform {
   required_version = ">= 1.2.0, < 1.3.0"
